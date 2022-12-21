f2b0154157470f782724e9beefdc5667b0ea6528
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-12-21 15:28:21 +0200

    Add Task component layout

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,5 +1,18 @@
 import { Task as TaskItem } from "../../features/tasks-slice";
 
 export const Task = ({ task }: { task: TaskItem }) => {
-  return <div className="py-3 border-b-2">{task.title}</div>;
+  return (
+    <section className="border-b-2 py-3">
+      <div className="flex justify-between">
+        <div className="">
+          <div>done icon</div>
+          <h2>{task.title}</h2>
+        </div>
+        <div className="">
+          <div>edit icon</div>
+          <div>delete icon</div>
+        </div>
+      </div>
+    </section>
+  );
 };
