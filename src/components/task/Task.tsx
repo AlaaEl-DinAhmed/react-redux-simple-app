f195040fe0bb65281016889db2ed4a26feb2caa2
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-12-21 17:06:44 +0200

    Add task component style

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,16 +1,21 @@
+import {
+  CheckCircleIcon,
+  PencilIcon,
+  TrashIcon,
+} from "@heroicons/react/24/solid";
 import { Task as TaskItem } from "../../features/tasks-slice";
 
 export const Task = ({ task }: { task: TaskItem }) => {
   return (
     <section className="border-b-2 py-3">
       <div className="flex justify-between">
-        <div className="">
-          <div>done icon</div>
+        <div className="flex">
+          <CheckCircleIcon className="icon mr-8" />
           <h2>{task.title}</h2>
         </div>
-        <div className="">
-          <div>edit icon</div>
-          <div>delete icon</div>
+        <div className="flex">
+          <PencilIcon className="icon mr-4" />
+          <TrashIcon className="icon" />
         </div>
       </div>
     </section>
